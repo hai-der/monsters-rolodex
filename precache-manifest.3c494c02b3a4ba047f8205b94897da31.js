258127deb8cdac7cd465fa3ded0c808fb4530015
Haider <hai.tiwana@gmail.com>
2020-01-19 15:31:22 -0800

    Updates

---
+++ precache-manifest.3c494c02b3a4ba047f8205b94897da31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4137731ed007675316421e51b82a0a5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d061bc470806c1c9b2e6",
+    "url": "/monsters-rolodex/static/css/main.cbedc505.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d061bc470806c1c9b2e6",
+    "url": "/monsters-rolodex/static/js/main.34661afd.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
